5c49f09aa3a7141a89301a927e168be6b6848a50
Alok Choudhary <alok.choudhary@e2enetworks.com>
2020-05-13 13:05:25 +0530

    Runtime: 0 ms, faster than 100.00%

---
+++ linkedlist/876.middle-of-linked-list.java
@@ -0,0 +1,33 @@
+
+// Given a non-empty, singly linked list with head node head, return a middle node of linked list.
+
+// If there are two middle nodes, return the second middle node.
+
+ 
+
+// Example 1:
+
+// Input: [1,2,3,4,5]
+// Output: Node 3
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
